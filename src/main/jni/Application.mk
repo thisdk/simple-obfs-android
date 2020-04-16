8ab32a59e467c716d16974f264b8f37859b0613b
Qixiong Yuan <thisdk@gmail.com>
2020-04-16 11:42:02 +0800

    Update Application.mk

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,4 +1,4 @@
 APP_ABI      			:= arm64-v8a
 APP_PLATFORM 			:= android-19
-APP_STL      			:= stlport_static
-NDK_TOOLCHAIN_VERSION 	:= clang
+APP_STL      			:= c++_static
+NDK_TOOLCHAIN_VERSION 	:= clang++
